c0473d8820e0a0d17ef2d697998b0c736398264c
Kaizoku22 <pdeepankar46@gmail.com>
2024-11-26 18:25:51 +0530

    Setup

---
+++ src/test/java/com/spring/project/PennyPlan/PennyPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.project.PennyPlan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PennyPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
